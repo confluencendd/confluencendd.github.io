998e18c4bbec8e43ee5ab062074ee39731d79643
confluencendd <confluence.ndd.analytics@gmail.com>
2024-11-19 16:55:53 -0300

    Build NDD Move

---
+++ src/nddmove/app-hc-nddmove.js
@@ -0,0 +1,33 @@
+// Importação das funções uteis para as rotinas
+import { getCurrentLanguage } from '../js/utils/getLanguage'
+import { getCurrentProduct } from '../js/utils/getProduct';
+import { redirectLinks } from '../js/utils/redirect';
+import { handleRoute } from '../js/utils/route'
+import { removeOldComponent } from '../js/utils/removeOldComponent'
+
+import { formatBreadcrumbs } from '../js/utils/breadcrumbs';
+import { createTimeToReadComponent } from '../js/components/header-bar/time-to-read';
+
+import { renderArticleFeedbackComponent } from '../js/components/article-feedback/article-feedback';
+
+// Parâmetros para execução das funções
+const languagePicked = getCurrentLanguage();
+const currentProduct = getCurrentProduct();
+const commonParams = { product: currentProduct, language: languagePicked };
+
+handleRoute({
+    functions: {
+        removeOldComponent,
+        renderArticleFeedbackComponent,
+        redirectLinks,
+        formatBreadcrumbs,
+        createTimeToReadComponent
+    },
+    params: {
+        renderArticleFeedbackComponentParams: { language: languagePicked },
+        redirectLinksParams: commonParams,
+        translatePlaceholderSearchBarParams: { language: languagePicked },
+        createTimeToReadComponentParams: { language: languagePicked }
+
+    }
+});
